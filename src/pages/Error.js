5c91bdc1e61ad63a7fbda1e3d5bfa89804263d3c
mancjames <multicolouredsmurf@gmail.com>
2022-02-13 22:16:10 +0000

    added Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom"
 
 export default class Error extends Component {
   render() {
     return (
-      <div>Error</div>
+      <section className="error">
+        <section className="error__hero">
+          <h1 className="error__title">404</h1>
+          <h5 className="error__text">Oops! It looks like this page doesn't exist</h5>
+        </section>
+        <p className="error__link"><Link to="/">Go back to the home page</Link></p>
+      </section>
     )
   }
 }
